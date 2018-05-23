aee3478b01a91de9e5420c27d020719609aca4cd
lekstrom <39048645+lekstrom@users.noreply.github.com>
2018-05-23 11:04:42 +0200

    Add files via upload
    bokningsbara tider

---
+++ display.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+
+<?php>
+
+	// måste kopplas till databas
+
+	include ...
+
+
+	$query = "SELECT Datum, StartTid, MassorID, Behandlingstyp FROM BokningsbarTid, Behandling WHERE BokningsbarTid.BehandlingsID=Behandling.BehandlingsID AND BestallarID IS NULL";
+	$result = 
+
+</php>
+
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<div id = "tider">
+			<h1>Tillgängliga tider:<h1>
+			<?php>
+
+				while($row = $result->fetch_assoc())
+				{
+					echo 	$row["Datum"] $row["StartTid"], $row["Behandlingstyp"], $row["MassörID"];
+					echo "<br/>";
+				}
+			
+			</php>
+		</div>
+		
+	</body>
+
+
+
+</html
