2d11ccfc7162ce96bda6c859ba52486b17f48029
avdpos <32863673+avdpos@users.noreply.github.com>
2018-05-24 11:10:54 +0200

    footer upp

---
+++ include/views/_footer.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+    <link rel="stylesheet" type="text/css" href="../../assets/css/_footer.css"> 
+    <ul id="meny">
+        <li><a href="contact.php">Contact us</a></li>
+    </ul>
+</body>
+</html>
