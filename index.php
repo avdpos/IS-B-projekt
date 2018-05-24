98e59777e0b881cd0d2c8dbaf9c07f6fcd8ce424
lekstrom <39048645+lekstrom@users.noreply.github.com>
2018-05-24 11:33:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,13 @@
 
 	<head>
 		<meta charset="UTF-8">
-		<title></title>
+		<title>MassageTid</title>
 		
 	</head>
 	<body>
 		<?php> include 'include/views/_header.php'?>
+		<?php> include 'include/modules/_datepicker.php'?>
+		
 		<?php> include 'include/views/_footer.php'?>
 		
 	</body>
