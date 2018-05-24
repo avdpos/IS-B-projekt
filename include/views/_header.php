bb320b8c1808afed1967c319b535b258453fa804
iihn <39034940+iihn@users.noreply.github.com>
2018-05-24 17:52:17 +0200

    Update _header.php

--- include/views/_header.php
+++ include/views/_header.php
@@ -4,44 +4,16 @@
 <body>
     <div class="title">
         <h1> Massagetid </h1>
-        
+
     </div>
-    <link rel="stylesheet" type="text/css" href="../../assets/css/_header.css"> 
+    <link rel="stylesheet" type="text/css" href="../../assets/css/_header.css">
     <ul id="meny">
         <li><a href="index.php">Home</a></li>
-        <li><a href="loggainmassage.html">Logga in beställare</a></li>
-        <li><a href="loggainmassor.hmtl">Logga in massör</a></li>
-        <li><a href="massorreg.php">REgistrera massör</a></li>
-        <li><a href="validateBestallarReg.php">reg bestallare</a></li>
-        <li><a href="addBokningsbarTid.php">Lägg till bokningsbar tid</a></li>
-
-        
-        <?php
-        if (session_status() == PHP_SESSION_NONE) {
-            session_start();
-        }
-
-        if (empty($_SESSION["loginstatus"]))
-        {
-           echo  "<li><a href='loggin.php'>Log in</a></li>";
-           
-        }
-        else if (!empty($_SESSION["loginstatus"]))
-        {
-            if ($_SESSION["loginstatus"] == TRUE)
-            {
-                echo "<li><a href='logout.php'>Log out</a></li>";
-                
-            }
-            else
-            {
-                echo  "<li><a href='loggin.php'>Log in</a></li>";
-                
-            }
-        }
-        ?>
-        <li><a href="registration.php">Registrate new account</a></li>
-
+        <li><a href="loggainmassor.php">Logga in massör</a></li>
+        <li><a href="loggainmassage.php">Logga in beställare</a></li>
+        <li><a href="massorreg.php">Registrera massör</a></li>
+        <li><a href="bestallreg.php">Registrera beställare</a></li>
+        <li><a href="addbokningsbarTid.php">Bokningsbara tider</a></li>
     </ul>
 </body>
 </html>
