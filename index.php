5444012611743bf630b30a2244d74195bb45f177
lekstrom <39048645+lekstrom@users.noreply.github.com>
2018-05-24 11:21:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+
+
+
+<html>
+
+
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		
+	</head>
+	<body>
+		<?php> include 'include/views/_header.php'?>
+		<?php> include 'include/views/_footer.php'?>
+		
+	</body>
+
+
+
+</html>
