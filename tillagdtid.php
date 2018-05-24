b178abba8981ac6e2aa2b5744194105dd458be4a
iihn <39034940+iihn@users.noreply.github.com>
2018-05-24 17:38:05 +0200

    Create tillagdtid.php

---
+++ tillagdtid.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/_header.css">
+    <title>Massagetid</title>
+</head>
+
+	<body>
+<br>
+
+		<div>
+				<?php include 'include/views/_header.php' ?>
+				</div>
+	</body>
+
+
+
+</html>
