5e7a2f59641dc7a200bb2996dc7be4d51b1c1bd1
avdpos <32863673+avdpos@users.noreply.github.com>
2018-05-24 11:04:35 +0200

    header up

--- experiment/index.php
+++ experiment/index.php
@@ -1,6 +1,7 @@
 <html>
 <head>   
 <link href="calendar.css" type="text/css" rel="stylesheet" />
+<link href="tumme.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
 <?php
@@ -13,5 +14,14 @@ echo $calendar->show();
 
 
 <input type="image" src="tummeupp.png" alt="Submit" width="100" height="100">
+
+<label>
+  <input type="radio" name="tummeupp" value="small" />
+  <img src="tummeupp.png">
+</label>
+<label>
+  <input type="radio" name="tummener" value="small" />
+  <img src="tummener.png">
+</label>
 </body>
 </html>       
---
+++ include/views/_header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+    <div class="title">
+        <h1> Massagetid </h1>
+        
+    </div>
+    <link rel="stylesheet" type="text/css" href="../../assets/css/_header.css"> 
+    <ul id="meny">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="loggainmassage.html">Logga in beställare</a></li>
+        <li><a href="loggainmassor.hmtl">Logga in massör</a></li>
+        <li><a href="massorreg.php">REgistrera massör</a></li>
+
+        
+        <?php
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (empty($_SESSION["loginstatus"]))
+        {
+           echo  "<li><a href='loggin.php'>Log in</a></li>";
+           
+        }
+        else if (!empty($_SESSION["loginstatus"]))
+        {
+            if ($_SESSION["loginstatus"] == TRUE)
+            {
+                echo "<li><a href='logout.php'>Log out</a></li>";
+                
+            }
+            else
+            {
+                echo  "<li><a href='loggin.php'>Log in</a></li>";
+                
+            }
+        }
+        ?>
+        <li><a href="registration.php">Registrate new account</a></li>
+
+    </ul>
+</body>
+</html>
