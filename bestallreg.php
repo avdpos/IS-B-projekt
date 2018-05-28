489ca5e31541e12bb1070f239962ed75a3113631
iihn <39034940+iihn@users.noreply.github.com>
2018-05-28 17:07:49 +0200

    Update bestallreg.php

--- bestallreg.php
+++ bestallreg.php
@@ -1,3 +1,4 @@
+         
 <!DOCTYPE html>
 <html>
 
@@ -5,51 +6,32 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" href="assets/css/_header.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Massagetid</title>
 </head>
 
 <body>
-  <br>
-	  <h3>Här registrerar beställare ny profil</h3>
-    <div>
+    <div class="wrapper">
+      <div>
         <?php include 'include/views/_header.php' ?>
         </div type="regForm">
-          <br>
-            <form method="post" onsubmit="return validateRegistration()" action="validateBestallarReg.php">
-                <table class="form">
-                    <tr>
-                        <th><label for="Namn">Namn</label></th>
-                        <td>
-                            <input type="text" name="Namn" id="Namn" required>
-
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><label for="Email">Email</label></th>
-                        <td>
-                            <input type="text" name="Email" id="Email"  required>
-
-                        </td>
-                    </tr>
-                    <tr>
-                        <th><label for="Password">Lösenord</label></th>
-                        <td>
-                            <input type="password" name="regPassword" id="regPassword" required>
-                        </td>
-                    <tr >
-                        <th><label for="Password2">Bekräfta lösenord</label></th>
-                        <td>
-                            <input type="password" name="regPassword2" id="regPassword2" required><br>
-
-                        </td>
-                    </tr>
-                </table>
-                <br>
-                <input type="submit" class="button" name="Submit" value="Registrera användare">
-            </form>
-        <div>
-    </div>
-    
+        
+        <h3>Här registreras beställare</h3>
+                <form method="post" onsubmit="return validateRegistration()" action="validateBestallarReg.php">
+        <div class="login-page">
+               <div class="form">
+                <form class="register-form"></form>
+                <form class="login-form">
+                    <input type="text" placeholder="Namn" name="Namn" id="namn" required>
+                    <input type="password" placeholder="Email" name="email" id="email" required>
+                    <input type="password" placeholder="Lösenord" name="regPassword2" id="regpassword1" required>
+                    <input type="password" placeholder="Bekräfta lösenord" name="regPassword2" id="regpassword1" required>
+                    <button>Skapa konto</button>
+                    <p class="message">Har du redan ett konto? <a href="loggainmassor.php">Logga in här!</a></p>
+                    <!--  <input type="submit" class="button" name="Submit" value="Registrera användare">-->
+              </form>
+             </div>
+           </div>
+    <script src="assets/js/bestallarRegistration.js"></script>
 </body>
-
 </html>
