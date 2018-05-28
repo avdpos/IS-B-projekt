bab2f9f44dbd29179cd959816fedfd0ee3f8a4e8
lekstrom <39048645+lekstrom@users.noreply.github.com>
2018-05-28 11:48:54 +0200

    Update searchTime.php

--- include/moduls/searchTime.php
+++ include/moduls/searchTime.php
@@ -2,7 +2,9 @@
 
 <?php
 
-$query= "SELECT * FROM massagetid, massoraddress WHERE massagetid.MassorID = massoraddress.MassorID AND Datum = sökn AND Postadress= sökn AND BestallareID IS NULL";
+include "dbconnection.php";
+
+$query= "SELECT * FROM massagetid, massoraddress WHERE massagetid.MassorID = massoraddress.MassorID AND Datum = sÃ¶kn AND Postadress= sÃ¶kn AND BestallareID IS NULL";
 
 $result = conn->query(query);
 
