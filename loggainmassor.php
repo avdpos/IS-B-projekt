479f39fd7a7eca02deaf8e1c24ca28917837ed41
iihn <39034940+iihn@users.noreply.github.com>
2018-05-28 16:31:34 +0200

    Update loggainmassor.php

--- loggainmassor.php
+++ loggainmassor.php
@@ -1,37 +1,32 @@
-<?php
 
-
- ?>
 <html>
 	<head>
-		<meta charset="utf-8">
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="assets/css/main.css">
 		<link rel="stylesheet" href="assets/css/_header.css">
-
-
-	</head>
+    		<link rel="stylesheet" href="assets/css/style.css">
+  	</head>
 
 <body>
-  <br>
-
+    <div class="wrapper">
 			<div>
-					<?php include 'include/views/_header.php' ?>
+				<?php include 'include/views/_header.php' ?>
 			</div>
-		</body>
-
-<br>
-    <h3>Här loggar massörer in</h3>
-		<div id="formular">
-				<label class="rubrik" for="mail">E-mail</label>
-				<input type="email" id="mail" name="mail" placeholder="E-mail">
-
-				<label class="rubrik" for="password">Lösenord</label>
-				<input type="password" id="password" name="password" placeholder="Lösenord">
-
-				<input id="knapp" type="submit" value="Logga in" name="submit" id="submitButton" class="button">
-			</form>
-		</div>
-
-	</body>
+      <h3>Här loggar massörer in</h3>
+      <div class="login-page">
+        <div class="form">
+         <form class="register-form">
+	</form>
+          <form class="login-form">
+		<input type="email" id="mail" name="mail" placeholder="E-mail">
+            	<input type="password" id="password" name="password" placeholder="Lösenord">   
+		<!--<input id="knapp" type="submit" value="Logga in" name="submit" id="submitButton" class="button">-->
+		  
+            <button>Logga in</button>
+            <p class="message">Har du inget konto? <a href="registreramassor.php">Skapa profil</a></p>
+          </form>
+        </div>
+      </div>
+    </div>
+</body>
 </html>
