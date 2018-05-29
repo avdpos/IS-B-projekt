453f983c88b7a6810d5c5c143812d4bb187fb032
lekstrom <39048645+lekstrom@users.noreply.github.com>
2018-05-29 10:42:12 +0200

    Update skrivrecension.php

--- skrivrecension.php
+++ skrivrecension.php
@@ -36,6 +36,11 @@ $result = $conn->query($query);
 					<label>Användarnamn:</label><br/>
 					<input type="text" id="username" name="username" required>
 					<br/><br/>
+					<label for="rekommenderas">Rekommenderas</label>
+					<input type="radio" id ="rekommenderas" name="betyg" value "Rekommenderas">
+					<label for="rekommenderas inte">Rekommenderas inte</label>									 
+					<input type="radio" id ="rekommenderas inte" name="betyg" value "Rekommenderas inte">
+					<br/><br/>							   
 					<textarea id="recension" name="recension" placeholder="Skriv din recension här..."></textarea>
 					<br/>
 					<input type="submit" value="Skicka" id="submit">
