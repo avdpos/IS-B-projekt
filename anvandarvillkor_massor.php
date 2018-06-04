291b3b7be767f081090d4a3c6bcd6845a67753eb
ronnysinjari <39048687+ronnysinjari@users.noreply.github.com>
2018-06-04 12:58:28 +0200

    Update anvandarvillkor_massor.php

--- anvandarvillkor_massor.php
+++ anvandarvillkor_massor.php
@@ -20,6 +20,9 @@
         <br><br><br><br><br>
         <?php
         //klistra in Massörens användarvilkor här under
+        Att ansluta sig till MassageTid som massör är kostnadsfritt och det är ingen bindningstid. Massörer får full tillgång att ta del av MassageTids samtliga tjänster. Debitering sker endast vid fullständiga bokningar. 
+        MassageTid ansvarar för att kontrollera och validera certifikaten hos massörer med certifikat som ansluter sig till tjänsten. Massörer med certifikat från Branschrådet Svensk Massage har förbundit sig till att följa de sociala och etiska regler som är skrivna av Branschrådet Svensk Massage 
+
         ?>
         
        <br><br><br>
