fac0ddcc1487d8b1802884c5b432b9d06a1b1a02
ronnysinjari <39048687+ronnysinjari@users.noreply.github.com>
2018-06-04 09:20:53 +0200

    Update anvandarvillkor_bestallare.php

--- anvandarvillkor_bestallare.php
+++ anvandarvillkor_bestallare.php
@@ -20,6 +20,9 @@
         <br><br><br><br><br>
         <?php
         //klistra in beställarnas användarvilkor här under
+      
+Beställaren ger samtycke till att MassageTid behandlar personuppgifter som ges av beställaren i samband med skapandet av ett konto. Dessa personuppgifter är namn, mailadress samt adress. Samtliga personuppgifter behandlas i syfte att MassageTid ska kunna bistå användaren av de tjänster man erbjudit i avtalet. Behandling av personuppgifter innebär, insamling, lagring ändring samt användning av personuppgifterna. Personuppgifterna måste behandlas enligt den nya dataskyddsförordningen GDPR. Detta för att säkerställa att användarnas personuppgifter behandlas på ett korrekt sätt som inte innebär att de kränker deras integritet.
+Beställare ska inte medvetet misskreditera MassageTid. Beställare ska hålla god ton och inte gå till personangrepp, använda ett ovårdat språk eller misskreditera massörer eller andra användare. Beställare ska endast registrera ett konto per användare. Detta för att inte belasta MassageTid med överflödig trafik samt begränsa möjligheterna för att manipulera betygssystemet.
         ?>
         
        <br><br><br>
